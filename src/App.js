4aecf3c0f67c884f15d4694a9b59becd2754f34b
Diego Hernandez <diegohmailbox@gmail.com>
2022-03-27 16:26:39 -0700

    Testing a different API

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
+import { Component } from "react";
 import "./App.css";
 import Body from "./Components/Body";
 import Header from "./Components/Header";
 import Searchbar from "./Components/Searchbar";
 import DrinkShow from "./Components/DrinkShow";
 import Footer from "./Components/Footer";
-import { Component } from "react";
+import mockdrinks from './MockDrinks'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drinks: [],
+      drinks: mockdrinks,
     };
   }
-
-  componentDidMount() {
-    this.readDrinks();
-  }
-
-  readDrinks = () => {
-    fetch("https://cocktail-recipes-tully4school.herokuapp.com/drinks")
-      .then((response) => {
-        return response.json();
-      })
-      .then((drinksArray) => {
-        this.setState({ drinks: drinksArray });
-      })
-      .catch((errors) => {
-        console.log("drinks read fetch errors", errors);
-      });
-  };
+  
 
   render() {
+
+    console.log(this.state.drinks);
     return (
       <>
         <div className="app">
           <Header />
           <Body />
           <Searchbar />
+          <button>View Drinks</button>
           <DrinkShow />
           <Footer />
-          <button onClick={this.readDrinks}>Search</button>
-          <p>Try these drinks {this.state.drinks.drinks}</p>
         </div>
       </>
     );
