e67d6399d30a9d53b4dd8174a426027b540bee49
Diego Hernandez <diegohmailbox@gmail.com>
2022-04-02 21:10:57 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import { Component } from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import Searchbar from "./Components/Searchbar";
+import Searchbar from "./Components/Searchbar"
 import Footer from "./Components/Footer";
+import mockdrinks from "./MockDrinks";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drinks: [],
-      pic: null,
-      // ingredients: '',
-      // instructions: '',
-      // searchField: ''
+      drinks: mockdrinks.drinkName,
+      name: "",
+      ingredients: "",
+      instructions: ""
     };
   }
 
-  componentDidMount(){
+  /*componentDidMount(){
     fetch("https://cocktail-recipes-tully4school.herokuapp.com/drinks")
     .then(response => response.json())
     .then(payload => this.setState({ drinks: payload }))
     .catch(error => console.log(error))
+  }*/
+
+  //Drink search result
+  // {drinks.map((value, key) => {
+  //   return <p className='drink-item'> {value.drinkName}</p>
+  // })}
+
+  randomDrinks = () => {
+    let randomNum = Math.floor(Math.random() * this.state.drinks.length)
+    this.setState({ name: this.state.drinks[randomNum].drinkName, ingredients: this.state.drinks[randomNum].drinkIngredients, instructions: this.state.drinks[randomNum].drinkInstructions })
   }
-
-
-  // randomDrinks = () => {
-  //   let randomNum = Math.floor(Math.random() * this.state.drinks.length)
-  //   this.setState({ pic: this.state.drinks[randomNum].drinkThumb, ingredients: this.state.drinks[randomNum].drinkIngredients, instructions: this.state.drinks[randomNum].drinkInstructions })
-  // }
 
   render() {
     // const{drinks, ingredients, instructions} = this.state
@@ -36,10 +40,9 @@ class App extends Component {
       <>
         <div className="app">
           <Header />
-          {/* <Searchbar placeholder="Search a flavor..." handleChange={(e) => this.setState({searchField:e.target.value})}/> */}
-
-          <p>These are the ingredients</p>
-          <p>This is how you make it</p>
+          <Searchbar placeholder="Search a flavor..."/>
+          <button onClick={this.randomDrinks}>View Random Drinks</button>
+          <p>{this.state.name}</p>
           <Footer />
         </div>
       </>
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../Stylesheets/Searchbar.css'
 
-const Searchbar = (props) => {
-  return (
-    <>
-    <div className='search-container'>
+export default class Searchbar extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <>
+      <div className='search-container'>
       <div className='search-input'>
-      <input type='search' className='input' id='search' placeholder={props.placeholder} onChange={props.handleChange}/>
+        <input type='text' 
+        className='input' 
+        id='search' 
+        placeholder={this.props.placeholder}/>
       </div>
     </div>
-    </>
-  )
+    <div className='drink-result'>
+
+    </div>
+      </>
+    )
+  }
 }
 
-export default Searchbar
+
+// const Searchbar = (placeholder, drinks) => {
+
+//   return (
+//     <>
+//     <div className='search-container'>
+//       <div className='search-input'>
+//         <input type='text' 
+//         className='input' 
+//         id='search' 
+//         placeholder="search"/>
+//       </div>
+//     </div>
+//     <div className='drink-result'>
+//       {/* {drinks.map((value, key) => {
+//         return <p className='drink-item'> {value.drinkName}</p>
+//       })} */}
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Searchbar
